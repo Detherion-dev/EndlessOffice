48b05d2af724671d8c6d2184955b5635347c42ce
Thomas Noiret <thomas.noiret.3@gmail.com>
2022-05-17 08:23:25 +0200

    Add Enumeration "actions" that contains all actions available in EndlessOffice

---
+++ src/main/java/endlessoffice/entity/action/actions.java
@@ -0,0 +1,30 @@
+package endlessoffice.entity.action;
+
+/**
+ * Enumerates all actions performed in EndlessOffice
+ */
+public enum actions {
+    MOVE,
+    EAT,
+    DRINK,
+    CODE,
+    DESIGN,
+    DEVELOP,
+    ORDERTO,
+    TALKTO,
+    WRITE,
+    READ,
+    REMEMBER,
+    BREAKITEM,
+    OPEN,
+    CLOSE,
+    TURNON,
+    TURNOFF,
+    PREPARETHECOFFEE,
+    TAKEITEM,
+    BADGE,
+    WATCH,
+    WAIT
+
+
+}
