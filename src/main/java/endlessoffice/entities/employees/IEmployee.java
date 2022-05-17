67ba0eaec6cde829aa11a258ee7b023fb0602d15
Thomas Noiret <thomas.noiret.3@gmail.com>
2022-05-17 10:55:14 +0200

    Remove "public" tag from methods

--- src/main/java/endlessoffice/entities/employees/IEmployee.java
+++ src/main/java/endlessoffice/entities/employees/IEmployee.java
@@ -13,23 +13,23 @@ public interface IEmployee {
      * Performs an action by the Employee
      * @param actionToExecute: action to execute
      */
-    public Action acts(Action actionToExecute);
+    Action acts(Action actionToExecute);
 
     /**
      * Adds a task in the Employee planning
      * @param task: task to add
      */
-    public void addTaskInPlanning(Action task);
+    void addTaskInPlanning(Action task);
 
     /**
      * Delete a task from the Employee planning
      * @param taskId: id of the task to delete
      */
-    public void deleteTaskFromPlanning(int taskId);
+    void deleteTaskFromPlanning(int taskId);
 
     /**
      * Update a task from the Employee planning
      * @param taskId: id of the task to update
      */
-    public void updateTaskFromPlanning(int taskId);
+    void updateTaskFromPlanning(int taskId);
 }
