611fa8dc0306187b71d18b45d0d61ce3bf1226a4
Thomas Noiret <thomas.noiret.3@gmail.com>
2022-05-17 08:45:03 +0200

    Add Enumeration "items" that enumerates all items in Endless Office

---
+++ src/main/java/endlessoffice/entity/item/items.java
@@ -0,0 +1,22 @@
+package endlessoffice.entity.item;
+
+public enum items {
+    DESK,
+    DRAWER,
+    COMPUTER,
+    SCREEN,
+    BAG,
+    STAPLER,
+    PEN,
+    NOTEPAD,
+    HEADPHONE,
+    BOOK,
+    BADGE,
+    MUG,
+    PHONE,
+    COFFEEMACHINE,
+    WATERDISPENSER,
+    FRIDGE,
+    KETTLE,
+
+}
