379cddd22831a5df97c2b85a22ee29cf97ff2c5a
Thomas Noiret <thomas.noiret.3@gmail.com>
2022-05-23 10:27:18 +0200

    - Empty the App main method - Add Items enumerator

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,7 @@
-import endlessoffice.gameloop.GameLoop;
 
 public class App {
 
     public static void main(String[] args) {
-        long startTime = System.currentTimeMillis();
-        GameLoop gp = new GameLoop();
-        gp.run();
-        long endTime = System.currentTimeMillis();
-        System.out.println("time calculation: " + (endTime - startTime) + " milliseconds");
+
     }
 }
