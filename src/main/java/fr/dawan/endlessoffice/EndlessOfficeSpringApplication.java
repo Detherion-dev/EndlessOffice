077b353d64a79a33c9ae66c722f4b111001e876f
Thomas Noiret <thomas.noiret.3@gmail.com>
2022-06-22 09:13:17 +0200

    Initial Commit

---
+++ src/main/java/fr/dawan/endlessoffice/EndlessOfficeSpringApplication.java
@@ -0,0 +1,13 @@
+package fr.dawan.endlessoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndlessOfficeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndlessOfficeSpringApplication.class, args);
+	}
+
+}
