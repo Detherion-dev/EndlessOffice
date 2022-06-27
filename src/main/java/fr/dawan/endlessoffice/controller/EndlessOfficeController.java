47d22bca64415c402819015072bec69cd0d1be84
Thomas Noiret <thomas.noiret.3@gmail.com>
2022-06-27 11:51:46 +0200

    Add Rest controller EndlessOfficeController

---
+++ src/main/java/fr/dawan/endlessoffice/controller/EndlessOfficeController.java
@@ -0,0 +1,20 @@
+package fr.dawan.endlessoffice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+;
+
+@RestController
+@RequestMapping("/endlessoffice")
+public class EndlessOfficeController {
+
+    @GetMapping("/login")
+    public ModelAndView login() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("base.html");
+        return modelAndView;
+    }
+}
