562e51465c517badfa1bc42a574e7e2101a52fd2
Thomas Noiret <thomas.noiret.3@gmail.com>
2022-07-05 09:50:10 +0200

    Implementation of Move and TakeItem test

---
+++ src/test/java/fr/dawan/endlessoffice/services/actions/MoveTest.java
@@ -0,0 +1,28 @@
+package fr.dawan.endlessoffice.services.actions;
+
+import fr.dawan.endlessoffice.entities.employees.Employee;
+import fr.dawan.endlessoffice.entities.employees.player.Player;
+import fr.dawan.endlessoffice.utils.enums.Position;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoveTest {
+
+    private Move move;
+
+    @BeforeEach
+    void setUp() {
+        move = new Move("moveToOfficeB", "moveToOfficeB", 1, 1);
+        move.addAction(new DropItem("drop Bag"));
+        move.setActor(new Player("Thomas", "Noiret"));
+    }
+
+    @Test
+    void execute() {
+        move.execute(Position.OFFICE_B);
+        Employee player = move.getActor();
+        assertEquals(Position.OFFICE_B, player.getPosition());
+    }
+}
